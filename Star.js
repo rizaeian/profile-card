90c1b25e83d7b98dd99895c438bf04f728407f64
Mohammd Jawid <76745982+rizaeian@users.noreply.github.com>
2023-01-17 22:39:43 +0600

    Create file Star.js

---
+++ Star.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Star(props){
+    
+    const starIcon = props.isFilled ? "star-filled.png" : "star-empty.png"
+    
+    return (
+        <div>
+            <img 
+                src={`../images/${starIcon}`} 
+                className="card--favorite"
+                onClick={props.hundleCLick}
+            />
+        </div>
+    )
+}
